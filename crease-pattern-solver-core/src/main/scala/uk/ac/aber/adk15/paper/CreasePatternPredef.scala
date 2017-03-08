1d45594beabca3c4b3f9c48c0a973d3c78ea908d
Adam <adamjking1995@outlook.com>
2017-03-08 16:47:43 +0000

    [DEV-7] Generally improved the naming and design of a lot of code. Introduced multiple layers into the foldable crease pattern representation. Updated tests to reflect this.

---
+++ crease-pattern-solver-core/src/main/scala/uk/ac/aber/adk15/paper/CreasePatternPredef.scala
@@ -0,0 +1,52 @@
+package uk.ac.aber.adk15.paper
+
+import uk.ac.aber.adk15.Point
+import uk.ac.aber.adk15.paper.PaperEdgeHelpers._
+
+object CreasePatternPredef {
+
+  type Layer = Set[PaperEdge[Point]]
+
+  object Layer {
+    def apply(edges: PaperEdge[Point]*): Layer     = edges.toSet
+    def apply(edges: Set[PaperEdge[Point]]): Layer = edges
+  }
+
+  object Constants {
+
+    object ModelConstants {
+
+      /**
+        * Represents a blank piece of paper with no folds.
+        * This blank paper is a square with four corners.
+        *
+        * 0,0        1,0
+        * +----------+
+        * |          |
+        * |          |
+        * |          |
+        * |          |
+        * +----------+
+        * 0,1        1,1
+        *
+        */
+      val BlankPaper: CreasePattern = CreasePattern from
+        (
+          Point(0, 0) -- Point(1, 0),
+          Point(1, 0) -- Point(1, 1),
+          Point(1, 1) -- Point(0, 1),
+          Point(0, 1) -- Point(0, 0)
+      )
+    }
+
+  }
+
+  object Helpers {
+
+    implicit class FoldedCreasePattern(val paperModel: CreasePattern) extends AnyVal {
+      @inline def <~~(paperEdge: PaperEdge[Point]): CreasePattern = paperModel fold paperEdge
+    }
+
+  }
+
+}
