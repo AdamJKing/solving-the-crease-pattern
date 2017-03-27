9238c9fc44656e448831ac5a6cef63169ab2065e
adamjking <adamjking1995@outlook.com>
2017-03-27 10:56:57 +0100

    [DEV-15] Added some code to show the file chooser

--- crease-pattern-solver-interface/src/main/scala/uk/ac/aber/adk15/controllers/ui/ApplicationViewController.scala
+++ crease-pattern-solver-interface/src/main/scala/uk/ac/aber/adk15/controllers/ui/ApplicationViewController.scala
@@ -1,24 +1,30 @@
 package uk.ac.aber.adk15.controllers.ui
 
+import java.io.File
 import java.util.concurrent.ForkJoinPool
 
 import uk.ac.aber.adk15.controllers.ApplicationController
-import uk.ac.aber.adk15.view.ConfigurationView
+import uk.ac.aber.adk15.view.{ApplicationView, ConfigurationView}
 
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
 import scalafx.scene.control.Alert
 import scalafx.scene.control.Alert.AlertType
+import scalafx.stage.FileChooser
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class ApplicationViewController(private val mainController: ApplicationController) {
+class ApplicationViewController(private val mainController: ApplicationController, private val creasePatternParser: CreasePatternParser) {
+
+  private var creasePatternFile: Option[File] = None
 
   def start(): Unit = {
     implicit val executionContext =
       ExecutionContext.fromExecutor(new ForkJoinPool(8))
 
-    mainController.execute() onComplete {
+    val creasePattern = creasePatternFile map creasePatternParser.parse(_)
+
+    mainController.execute(creasePattern) onComplete {
       case Success(Some(result)) => // show result view
       case Success(None)         => // show condolence view
       case Failure(ex) =>
@@ -34,5 +40,8 @@ class ApplicationViewController(private val mainController: ApplicationControlle
     ConfigurationView.show()
   }
 
-  def loadCreasePattern(): Unit = {}
+  def loadCreasePattern(): Unit = {
+    val fileChooser = new FileChooser
+    creasePatternFile = Option(fileChooser.showOpenDialog(ApplicationView))
+  }
 }
